317959fa00511db7346ebc73aed7dc489203267c
VijayaprakashVp <prakashvijay905@gmail.com>
2022-03-29 19:13:19 +0530

    inital check

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
-const app = require('../index');
+const app = require("../index");
 
-const connect = require('./Configs/db');
+const connect = require("./Configs/db");
 
 app.listen(8080, async () => {
   await connect();
-  console.log('Listining to the  port 8080');
+  console.log("Listining to the  port 8080");
 });
