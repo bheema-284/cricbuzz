1edb035540f04a83431ab52bf0c6faa00d2e41d2
Niranjan <narayana9492324464@gmail.com>
2022-03-29 17:57:32 +0530

    react adaptability puspose

--- frontend/cricbuzz/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return <div className="App">Wellcome to cricbuzz</div>;
-}
-
-export default App;
