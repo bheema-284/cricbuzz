ce1a39e4e28c06c4b52d5bc73700b26cecda0927
bheema-284 <bheemudug@gmail.com>
2022-03-29 11:15:54 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+module.exports = app;
---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+const app = require('../index');
+
+const connect = require('./Configs/db');
+
+app.listen(8080, async () => {
+  await connect();
+  console.log('Listining to the  port 8080');
+});
