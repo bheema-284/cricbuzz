0d2444e5c5903b1e5048dd162196fcbae2b235f2
VijayaprakashVp <prakashvijay905@gmail.com>
2022-04-01 17:46:55 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,11 +8,7 @@ const port = process.env.PORT || 8080;
 app.listen(port, async () => {
   try {
     await connect();
-<<<<<<< Updated upstream
     console.log("Listening to the port 8080");
-=======
-    console.log("Listening to the  port 8080");
->>>>>>> Stashed changes
   } catch (e) {
     console.log(e.message);
   }
